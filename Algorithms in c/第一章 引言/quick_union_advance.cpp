44d5c22eae7b056f637df3b748dce4eee694da4d
Administrator <gestapo1888888@gmail.com>
2019-09-09 10:04:21 +0800

    1.3

---
+++ Algorithms in c/第一章 引言/quick_union_advance.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define N 1000
+
+int main()
+{
+	int i, p, q, j, id[N],sz[N];
+	for (i = 0; i < N; i++) 
+	{
+		id[i] = i;sz[i] = 1;
+	}
+	while (scanf("%d %d", &p, &q) == 2)
+	{
+		for (i = p; i != id[i]; i = id[i]);
+		for (j = q; j != id[j]; j = id[j]);
+		if (i == j) continue;
+		if(sz[i] < sz[j])
+		{
+			id[i] = j; sz[j] += sz[i];
+		}
+		else
+		{
+			id[j] = i; sz[i] += sz[j];
+		}
+
+		printf(" %d %d \n", p, q);
+	}
+}
